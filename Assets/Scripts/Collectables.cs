8562b5b0370c9f06df42fece42237648536912c8
Elad Machluf <eladmahluf@gmail.com>
2022-11-19 14:06:55 +0200

    Project Files
    Added Audio

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
 
 public class Collectables : MonoBehaviour
 {
-    
+    public AudioClip collectSound;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Destroy(this.gameObject);
-        }  
+            audioSource.clip = collectSound;
+            audioSource.Play();
+        }
+        Destroy(this.gameObject);
     }
+
 
 
 }
