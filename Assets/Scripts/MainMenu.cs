d28c72a490649c26a969cfcefb5f3f34f39fd725
Elad Machluf <eladmahluf@gmail.com>
2022-11-22 21:46:48 +0200

    Project Files

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -19,11 +19,6 @@ public class MainMenu : MonoBehaviour
         StartCoroutine(newGameTextRoutine());  
     }
 
-    private void Update()
-    {
-        
-
-    }
 
     public void LoadGame()
     {
