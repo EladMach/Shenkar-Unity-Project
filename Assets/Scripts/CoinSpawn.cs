1265f655f5378007ece5bbfb4a1ec7606f89fd6a
Elad Machluf <eladmahluf@gmail.com>
2022-11-23 19:15:55 +0200

    Revert "Project Files"
    This reverts commit 2d1cdca72be9c0f4e8c2a7ce01a39f7c9392feab.

--- Assets/Scripts/CoinSpawn.cs
+++ Assets/Scripts/CoinSpawn.cs
@@ -12,16 +12,20 @@ public class CoinSpawn : MonoBehaviour
         StartCoroutine(CoinSpawnRoutine());
     }
 
+    private void Update()
+    {
+        
+    }
 
     IEnumerator CoinSpawnRoutine()
     {
         while (_stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-13f, 25f), 0.5f, (Random.Range(-10f, 26f)));
-            Destroy(Instantiate(coinPrefabe, posToSpawn, Quaternion.identity), 10f);
+            Instantiate(coinPrefabe, posToSpawn, Quaternion.identity);
             yield return new WaitForSeconds(3.0f);
         }
-        
+
     }
     
 }   
