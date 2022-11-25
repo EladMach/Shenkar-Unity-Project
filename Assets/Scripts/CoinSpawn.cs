5a70ae923989f6f6d497c0f60274c3a8539dc17e
Elad Machluf <eladmahluf@gmail.com>
2022-11-25 09:05:16 +0200

    Project Files

--- Assets/Scripts/CoinSpawn.cs
+++ Assets/Scripts/CoinSpawn.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CoinSpawn : MonoBehaviour
 {
     public GameObject coinPrefabe;
     private bool _stopSpawning = false;
+    
 
     void Start()
     {
@@ -21,7 +23,7 @@ public class CoinSpawn : MonoBehaviour
     {
         while (_stopSpawning == false)
         {
-            Vector3 posToSpawn = new Vector3(Random.Range(-13f, 25f), 0.5f, (Random.Range(-10f, 26f)));
+            Vector3 posToSpawn = new Vector3(Random.Range(-13f, 25f), 2f, (Random.Range(-10f, 26f)));
             Destroy(Instantiate(coinPrefabe, posToSpawn, Quaternion.identity), 8.0f);
             yield return new WaitForSeconds(3.0f);
         }
